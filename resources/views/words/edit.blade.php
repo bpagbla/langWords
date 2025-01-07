f9705507e33c18a3856683f4a8e335781f660342
bpagbla <pagesbeatriz@gmail.com>
2025-01-08 00:33:07 +0100

    edit form frontend

--- resources/views/words/edit.blade.php
+++ resources/views/words/edit.blade.php
@@ -10,31 +10,45 @@
 
 </head>
 
-<body class="m-4">
+<body>
     <h1>Edit Word</h1>
+    <div class="col-md-8 mx-auto">
+        <!-- Formulario de edición -->
+        <form action="{{ route('words.update', $word->id) }}" method="POST" class="row g-3">
+            @csrf
+            @method('PUT')
+            <div class="col-md-6">
 
-    <!-- Formulario de edición -->
-    <form action="{{ route('words.update', $word->id) }}" method="POST">
-        @csrf
-        @method('PUT')
+                <label for="wordFirstLang" class="form-label">Word/Phrase (First Language):</label>
+                <input type="text" class="form-control" name="wordFirstLang" value="{{ $word->wordFirstLang }}"
+                    required><br><br>
+            </div>
+            <div class="col-md-6">
 
-        <label for="wordFirstLang">Word/Phrase (First Language):</label>
-        <input type="text" name="wordFirstLang" value="{{ $word->wordFirstLang }}" required><br><br>
+                <label for="wordSecondLang" class="form-label">Translation (Second Language):</label>
+                <input type="text" class="form-control" name="wordSecondLang" value="{{ $word->wordSecondLang }}"
+                    required><br><br>
+            </div>
+            <div class="col-md-6">
 
-        <label for="wordSecondLang">Translation (Second Language):</label>
-        <input type="text" name="wordSecondLang" value="{{ $word->wordSecondLang }}" required><br><br>
+                <label for="sentenceFirstLang" class="form-label">Example Sentence (First Language):</label>
+                <textarea class="form-control"
+                    name="sentenceFirstLang">{{ $word->sentenceFirstLang }}</textarea><br><br>
+            </div>
+            <div class="col-md-6">
 
-        <label for="sentenceFirstLang">Example Sentence (First Language):</label>
-        <textarea name="sentenceFirstLang">{{ $word->sentenceFirstLang }}</textarea><br><br>
+                <label for="sentenceSecondLang" class="form-label">Example Sentence (Second Language):</label>
+                <textarea class="form-control"
+                    name="sentenceSecondLang">{{ $word->sentenceSecondLang }}</textarea><br><br>
+            </div>
+            <div class="col-md-3">
+            <button type="submit" class="btn btn-primary mb-3">Update</button></div>
+        </form>
 
-        <label for="sentenceSecondLang">Example Sentence (Second Language):</label>
-        <textarea name="sentenceSecondLang">{{ $word->sentenceSecondLang }}</textarea><br><br>
-
-        <button type="submit">Update</button>
-    </form>
-
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
